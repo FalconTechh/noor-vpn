6427e0b6f1ba362baa3ca829ed1541bc3b95a1a9
FalconTechh <kanhagirihspt1@gmail.com>
2026-08-16 19:28:50 +0400

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,27 @@
+import 'package:flutter/material.dart';
+import 'theme/app_theme.dart';
+import 'screens/splash_screen.dart';
+import 'services/ad_service.dart';
 
+// Splash now routes to LoginScreen (see splash_screen.dart), which itself
+// routes to HomeScreen either after Google Sign-In or "Continue as Guest".
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await AdService.instance.init();
+  runApp(const NoorVpnApp());
+}
+
+class NoorVpnApp extends StatelessWidget {
+  const NoorVpnApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Noor VPN',
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme.dark,
+      home: const SplashScreen(),
+    );
+  }
+}
